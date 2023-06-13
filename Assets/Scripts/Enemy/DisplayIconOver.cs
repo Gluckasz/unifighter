80611ff859c4771292a9518de0dc5a8d3a946e2a
Lukasz <lukas.gluck.04@gmail.com>
2023-06-13 22:17:46 +0200

    Added graphics

---
+++ Assets/Scripts/Enemy/DisplayIconOver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayIconOver : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        this.enabled = false;
+    }
+}
