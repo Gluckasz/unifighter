276868851f3709eb37c7af1601782c6fd21a1543
Lukasz <lukas.gluck.04@gmail.com>
2023-06-14 11:06:28 +0200

    added horny stacks visuals

--- Assets/Scripts/Enemy/HornyStacks.cs
+++ Assets/Scripts/Enemy/HornyStacks.cs
@@ -7,11 +7,15 @@ public class HornyStacks : MonoBehaviour
 {
     private HealthManager enemyHealthManagerScript;
     private EndTurnManager endTurnManagerScript;
+    private GameObject player;
+    private Transform playerTransform;
     public int hornyStack = 0;
     private void Awake()
     {
         endTurnManagerScript = GameObject.FindGameObjectWithTag("EndTurnManager").GetComponent<EndTurnManager>();
         enemyHealthManagerScript = GameObject.FindGameObjectWithTag("Enemy").GetComponent<HealthManager>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.transform;
     }
     private void OnEnable()
     {
@@ -22,10 +26,27 @@ public class HornyStacks : MonoBehaviour
         }
         if (gameObject.GetComponent<EnemyAttack>() != null )
         {
+            for (int i = 0; i < playerTransform.childCount; i++)
+            {
+                Transform childTransform = playerTransform.GetChild(i);
+                if (childTransform.gameObject.tag == "hornStack" && !childTransform.gameObject.activeSelf)
+                {
+                    childTransform.gameObject.SetActive(true);
+                    break;
+                }
+            }
             hornyStack++;
         }
         if (hornyStack > 4)
         {
+            for (int i = 0; i < playerTransform.childCount; i++)
+            {
+                Transform childTransform = playerTransform.GetChild(i);
+                if (childTransform.gameObject.tag == "hornStack")
+                {
+                    childTransform.gameObject.SetActive(false);
+                }
+            }
             hornyStack = 0;
             endTurnManagerScript.setEnergy = 1;
         }
